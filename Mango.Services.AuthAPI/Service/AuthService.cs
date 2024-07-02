1c3f05af362f9bbe7d3ca5760a1fd1a2df6a506f
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-07-02 21:09:30 +0200

    Token in action with error to fix

--- Mango.Services.AuthAPI/Service/AuthService.cs
+++ Mango.Services.AuthAPI/Service/AuthService.cs
@@ -11,10 +11,12 @@ namespace Mango.Services.AuthAPI.Service
         private readonly AppDbContext _db;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
 
-        public AuthService(AppDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        public AuthService(AppDbContext db, IJwtTokenGenerator jwtTokenGenerator, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             _db = db;
+            _jwtTokenGenerator = jwtTokenGenerator;
             _userManager = userManager;
             _roleManager = roleManager;
         }
@@ -30,6 +32,7 @@ namespace Mango.Services.AuthAPI.Service
                 return new LoginResponseDto() {User=null,Token=""};
             }
             // if user was found generate jwt token
+            var token = _jwtTokenGenerator.GenerateToken(user);
 
             UserDto userDTO = new()
             {
@@ -42,7 +45,7 @@ namespace Mango.Services.AuthAPI.Service
             LoginResponseDto loginResponseDto = new LoginResponseDto()
             {
                 User = userDTO,
-                Token = ""
+                Token = token
             };
 
             return loginResponseDto;
--- Mango.Services.AuthAPI/Service/JwtTokenGenerator.cs
+++ Mango.Services.AuthAPI/Service/JwtTokenGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using Mango.Services.AuthAPI.Models;
 using Mango.Services.AuthAPI.Service.IService;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -10,9 +11,16 @@ namespace Mango.Services.AuthAPI.Service
     public class JwtTokenGenerator : IJwtTokenGenerator
     {
         private readonly JwtOptions _jwtOptions;
-        public JwtTokenGenerator(JwtOptions jwtOptions) 
+        public JwtTokenGenerator(IOptions<JwtOptions> jwtOptions) 
         { 
-            _jwtOptions = jwtOptions;
+            _jwtOptions = jwtOptions.Value;
+
+            // Validate the key size
+            var keyBytes = Encoding.ASCII.GetBytes(_jwtOptions.Secret);
+            if (keyBytes.Length < 16) // 16 bytes = 128 bits
+            {
+                throw new ArgumentOutOfRangeException("The key size must be at least 128 bits (16 bytes).");
+            }
         }
         public string GenerateToken(ApplicationUser applicationUser)
         {
