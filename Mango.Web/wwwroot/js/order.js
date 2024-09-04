0c3efeabaa8fd62ff90771f5e7b5fe90fdb944bb
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-09-04 19:17:43 +0200

    order endpoints in ocelot

--- Mango.Web/wwwroot/js/order.js
+++ Mango.Web/wwwroot/js/order.js
@@ -25,12 +25,17 @@ function loadDataTable(status) {
         order: [[0, 'desc']],
         "ajax": { url: "/order/getall?status=" + status },
         "columns": [
-            { data: 'orderHeaderId', "width": "5%"},
+            { data: 'orderHeaderId', "width": "5%" },
             { data: 'email', "width": "25%" },
             { data: 'name', "width": "20%" },
             { data: 'phone', "width": "10%" },
             { data: 'status', "width": "10%" },
-            { data: 'orderTotal', "width": "10%" },
+            {
+                data: 'orderTotal',
+                "render": (data) => +data.toFixed(2),
+                "width": "10%",
+
+            },
             {
                 data: 'orderHeaderId',
                 "render": function (data) {
